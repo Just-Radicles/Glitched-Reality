68bae385e856c620469f03f6ea0a554e36732060
Just-Radicles <amynguyen819@u.boisestate.edu>
2023-04-24 16:41:37 -0600

    Condensed scripts and made some adjustments

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChestChangeScene : MonoBehaviour
+public class ChangeScene : MonoBehaviour
 {
 
     public string sceneName;
